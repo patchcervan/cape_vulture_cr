711ed6be94f3b3028a5b6fd19b8dbabd73497a3d
patchcervan <francerper@gmail.com>
2021-01-05 18:43:39 +0200

    Processing mb birds uses .rds instead of .csv, plus other fixes

--- R/1_data_processing/1e_pre_process_vp_mb.R
+++ R/1_data_processing/1e_pre_process_vp_mb.R
@@ -62,7 +62,7 @@ for(i in 1:7){
                error_3d = NA) %>% 
         select(colnames(bird_trk))
     
-    write_csv(new_trk, path = paste("data/working/pre_proc_data/trk_", bird_id,"_pp.csv", sep = ""))
+    saveRDS(new_trk, file = paste("data/working/pre_proc_data/trk_", bird_id,"_pp.rds", sep = ""))
     
     
     # Fill in track template --------------------------------------------------
@@ -94,7 +94,7 @@ for(i in 1:7){
             # ring id
             ring_id = ring_id,
             # capture date
-            date_start = as.POSIXct(strftime(new_trk$datetime[1], format = "%m/%d/%y"), tz = "GMT", format = "%m/%d/%y"),
+            date_start = date(new_trk$datetime[1]),
             # date of last location
             date_end = NA,
             # name of the bird
@@ -115,7 +115,7 @@ for(i in 1:7){
             sd_dt = NA ) %>% 
         select(colnames(bird_db))
     
-    write_csv(new_db, path = paste("data/working/pre_proc_data/db_", bird_id,"_pp.csv", sep = ""))
+    saveRDS(new_db, file = paste("data/working/pre_proc_data/db_", bird_id,"_pp.rds", sep = ""))
     
     
 }
