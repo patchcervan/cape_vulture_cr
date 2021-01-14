f56e90e53cc2f93eea08e2d2455045b07391cbc1
patchcervan <francerper@gmail.com>
2021-01-14 10:04:14 +0200

    Joined height plot and process scripts

--- R/1_data_processing/6_height_process.R
+++ R/1_data_processing/6_height_process.R
@@ -1,6 +1,6 @@
 # 16-10-2020
 
-# In this script we process height data from all vultures selected from the data base to
+# In this script we plot height data from all vultures selected from the data base to
 # fit a hierarchical Ornstein-Uhlenbeck process model to the height readings
 
 rm(list = ls())
@@ -13,34 +13,113 @@ library(raster)
 # Read in data ------------------------------------------------------------
 
 # List birds in the database
-trk_files <- list.files("data/working/bird_tracks/fit_ready", pattern = ".csv")
-
-# Read in bird data base
-bird_db <- read_csv("data/working/bird_db_fit_ready.csv")
+trk_files <- list.files("data/working/bird_tracks/in_process", pattern = ".rds")
 
 # Function to extract covariates from rasters
 source("R/functions/extractCovts.R")
+
 
 # Process tracks ----------------------------------------------------------
 
 # for each bird
 for(i in 1:length(trk_files)){
    
-   # i = 1
+   trk <- readRDS(paste0("data/working/bird_tracks/in_process/", trk_files[i]))
+   
+   id_sel <- unique(trk$bird_id)
+   print(id_sel)
+   
+   # Check previous processing steps
+   if(length(attr(trk, "move")) == 0){
+      print("No move information?")
+      next
+   }
+   
+   # Plot altitude data
+   trk %>% 
+      ggplot(aes(x = datetime, y = alt)) +
+      geom_point()
+   
+   trk %>% 
+      ggplot(aes(x = alt)) +
+      geom_histogram()
+   
+   
+   # Create sf object and reproject
+   trk <- st_as_sf(trk, coords = c("lon", "lat"), crs = 4326, remove = F)
+   
+   # Extract topographical covariates
+   trk <- trk %>% 
+      extractCovts(loadCovtsPath = "R/functions/loadCovtsRasters.R",
+                   extractCovtPath = "R/functions/extractCovt.R",
+                   covts_path = "data/working/covts_rasters",
+                   covts_names = "srtm0",
+                   return_sf = FALSE, extract_method = "merge")
+   
+   # Substract GPS altitude from ground altitude
+   trk <- trk %>% 
+      mutate(height = alt - srtm0)
+   
+   # Plot height data
+   ppt <- trk %>% 
+      ggplot(aes(x = datetime, y = height)) +
+      geom_point(alpha = 0.5) +
+      ggtitle(id_sel)
+   
+   hst <- trk %>% 
+      ggplot(aes(x = height)) +
+      geom_histogram() +
+      ggtitle(id_sel)
+
+   ggsave(filename = paste0("output/height_plots/hgt_ppt_", id_sel,".png"), ppt)
+   ggsave(filename = paste0("output/height_plots/hgt_hst_", id_sel,".png"), hst)
+   
+   # Add attribute height
+   attr(trk, "height") <- 1
+   
+   # Save bird
+   saveRDS(trk, file = paste0("data/working/bird_tracks/in_process/", id_sel, ".rds"))
+}
+
+
+# DO NOT RUN:
+
+# Process tracks ----------------------------------------------------------
+
+# for each bird
+for(i in 1:length(trk_files)){
    
    trk <- read_csv(paste0("data/working/bird_tracks/fit_ready/", trk_files[i]))
    
    id_sel <- unique(trk$bird_id)
    print(id_sel)
    
+   # Check previous processing steps
+   if(length(attr(trk, "height")) == 0){
+      print("No height information?")
+      next
+   }
+   
    # Plot altitude data
    trk %>% 
-      ggplot(aes(x = t_, y = alt)) +
+      ggplot(aes(x = t_, y = height)) +
       geom_point()
    
-   trk %>% 
-      ggplot(aes(x = alt)) +
-      geom_histogram()
+   if(id_sel == "ez01"){
+      
+      trk <- trk %>% 
+         mutate(hat_hgt = if_else(height < -300, height + 2042, height))
+      
+      trk %>% 
+         ggplot(aes(x = t_, y = hat_hgt)) +
+         geom_point()
+      
+      
+   } else {
+      next
+   }
+   
+   
    
    
    # Create sf object and reproject
@@ -59,16 +138,17 @@ for(i in 1:length(trk_files)){
       mutate(height = alt - srtm0)
    
    # Plot height data
-   ppt <- trk %>% 
+   trk %>% 
       ggplot(aes(x = t_, y = height)) +
-      geom_point(alpha = 0.5) +
-      ggtitle(id_sel)
+      geom_point()
    
-   hst <- trk %>% 
-      ggplot(aes(x = height)) +
-      geom_histogram() +
-      ggtitle(id_sel)
-
-   ggsave(filename = paste0("figures/height_plots/hgt_ppt_", id_sel,".png"), ppt)
-   ggsave(filename = paste0("figures/height_plots/hgt_hst_", id_sel,".png"), hst)
+   if(id_sel == "ez01"){
+      
+   }
+   
+   
+   
+   # Save bird
+   write_csv(trk, file = paste0("data/working/bird_tracks/fit_ready/", id_sel, "_height.csv"))
+   
 }
--- R/1_data_processing/7_create_db_fit_ready.R
+++ R/1_data_processing/7_create_db_fit_ready.R
@@ -15,7 +15,13 @@ library(lubridate)
 bird_db_all <- read_csv("data/working/bird_db.csv")
 
 # List all tracking file names that are model ready
-trk_files <- list.files("data/working/bird_tracks/fit_ready", pattern = ".csv")
+trk_files <- list.files("data/working/bird_tracks/in_process/", pattern = "mxt.rds")
+
+# Read in problems with bird locations
+loc_issues <- read_csv("data/working/problem_birds_locs.csv")
+
+# Read in birds that are excluded because of movement modes
+mov_issues <- read_csv("data/working/birds_mxt_exclude.csv")
 
 # Create new data base to store birds that are model fit ready.
 # We will add the median of dt because it is usually more representative than the mean.
@@ -29,17 +35,19 @@ new_db <- bird_db_all %>%
 
 for(i in 1:length(trk_files)){
     
-    # i <- 1
-    
     # Read in track file
-    trk <- read_csv(paste0("data/working/bird_tracks/fit_ready/", trk_files[i]))
+    trk <- readRDS(paste0("data/working/bird_tracks/in_process/", trk_files[i]))
     
     # Define bird id
     id_sel <- unique(trk$bird_id)
     
-    # Compute time increments between locations
-    trk <- trk %>% 
-        mutate(dt = as.numeric(difftime(lead(t_), t_, units = "hours")))
+    # Find problems with locs
+    l <- loc_issues %>% 
+        filter(bird_id == id_sel)
+    
+    # Find problems with movement modes
+    m <- mov_issues %>% 
+        filter(bird_id == id_sel)
     
     # Extract bird from database with all birds
     new_record <- bird_db_all %>% 
