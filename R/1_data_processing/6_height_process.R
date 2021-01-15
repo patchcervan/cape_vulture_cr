31f6c27752564466b5ee01767632edb9da126fb8
patchcervan <francerper@gmail.com>
2021-01-15 08:57:42 +0200

    Fixed birds with "wrapping" heights

--- R/1_data_processing/6_height_process.R
+++ R/1_data_processing/6_height_process.R
@@ -1,7 +1,6 @@
 # 16-10-2020
 
-# In this script we plot height data from all vultures selected from the data base to
-# fit a hierarchical Ornstein-Uhlenbeck process model to the height readings
+# In this script we plot and process height data from all vultures
 
 rm(list = ls())
 
@@ -44,7 +43,6 @@ for(i in 1:length(trk_files)){
       ggplot(aes(x = alt)) +
       geom_histogram()
    
-   
    # Create sf object and reproject
    trk <- st_as_sf(trk, coords = c("lon", "lat"), crs = 4326, remove = F)
    
@@ -73,6 +71,86 @@ for(i in 1:length(trk_files)){
 
    ggsave(filename = paste0("output/height_plots/hgt_ppt_", id_sel,".png"), ppt)
    ggsave(filename = paste0("output/height_plots/hgt_hst_", id_sel,".png"), hst)
+   
+
+   # Make corrections --------------------------------------------------------
+
+   # Plot altitude and height
+   trk %>% 
+      dplyr::select(datetime, alt, height) %>% 
+      gather(measure, value, -datetime) %>% 
+      ggplot(aes(x = datetime, y = value)) +
+      geom_point() +
+      facet_wrap("measure", nrow = 2, scales = "free")
+   
+   # Plot altitude and height
+   trk %>% 
+      dplyr::select(datetime, alt, height) %>% 
+      gather(measure, value, -datetime) %>% 
+      ggplot(aes(x = value)) +
+      geom_histogram(aes(y = ..density..)) +
+      geom_density(colour = "red") +
+      facet_wrap("measure", nrow = 2, scales = "free")
+   
+   # Plot change in height vs. height after the change
+   trk %>% 
+      mutate(dalt = alt - lag(alt)) %>% 
+      ggplot(aes(x = alt, y = dalt)) +
+      geom_point()
+   
+   trk %>% 
+      mutate(dheight = height - lag(height)) %>% 
+      ggplot(aes(x = height, y = dheight)) +
+      geom_point()
+   
+   trk %>% 
+      mutate(dheight = height - lag(height),
+             ddheight = dheight - lag(dheight)) %>% 
+      ggplot(aes(x = height, y = ddheight)) +
+      geom_point()
+   
+   # Based on the previous plots correct altitude
+   trk <- trk %>% 
+      mutate(alt_c = case_when(id_sel == "ez01" & height < -500 ~ alt + 2042,
+                               id_sel == "ez04" & alt < 900 ~ alt + 2042,
+                               id_sel == "ez06" & height < -200 ~ alt + 2042,
+                               id_sel == "ez07" & height < -200 ~ alt + 2042,
+                               id_sel == "na01" & height < -200 ~ alt + 2042,
+                               id_sel == "na02" & height < -200 ~ alt + 2042,
+                               id_sel == "na03" & height < -250 ~ alt + 2042,
+                               id_sel == "na04" & height < -200 ~ alt + 2042,
+                               id_sel == "na05" & height < -200 ~ alt + 2042,
+                               id_sel == "na06" & height < -200 ~ alt + 2042,
+                               id_sel == "na07" & height < -200 ~ alt + 2042,
+                               id_sel == "na08" & alt < 850 ~ alt + 2042,
+                               id_sel == "na09" & height < -200 ~ alt + 2042,
+                               TRUE ~ alt))
+   
+   trk %>% 
+      ggplot(aes(x = datetime, y = alt_c)) +
+      geom_point()
+   
+   # Substract GPS altitude from ground altitude
+   trk <- trk %>% 
+      mutate(height = alt_c - srtm0)
+   
+   # if height was corrected plot the corrected height
+   if(id_sel %in% c("ez01", "ez04", "ez06", "ez07", "na01", "na02", "na03", "na04", "na05", "na06", "na07", "na08", "na09")){
+      
+      ppt <- trk %>% 
+         ggplot(aes(x = datetime, y = height)) +
+         geom_point(alpha = 0.5) +
+         ggtitle(id_sel)
+      
+      hst <- trk %>% 
+         ggplot(aes(x = height)) +
+         geom_histogram() +
+         ggtitle(id_sel)
+      
+      ggsave(filename = paste0("output/height_plots/hgt_ppt_", id_sel,"_fix.png"), ppt)
+      ggsave(filename = paste0("output/height_plots/hgt_hst_", id_sel,"_fix.png"), hst)
+      
+   }
    
    # Add attribute height
    attr(trk, "height") <- 1
