5143e2f945fca8e54732cf3c26d1d1156d44a22d
patchcervan <francerper@gmail.com>
2021-01-22 15:20:14 +0200

    Fixed included/excluded categories

--- R/1_data_processing/8_create_db_fit_ready.R
+++ R/1_data_processing/8_create_db_fit_ready.R
@@ -1,4 +1,4 @@
-# 1-10-2020
+# 10-01-2020
 
 # In this script we create a data base summary of the data that is ready
 # for model fit.
@@ -15,7 +15,7 @@ library(lubridate)
 bird_db_all <- read_csv("data/working/bird_db.csv")
 
 # List all tracking file names that are model ready
-trk_files <- list.files("data/working/bird_tracks/in_process/", pattern = "mxt.rds")
+trk_files <- list.files("data/working/bird_tracks/in_process/", pattern = ".rds")
 
 # Read in problems with bird locations
 loc_issues <- read_csv("data/working/problem_birds_locs.csv")
@@ -31,7 +31,6 @@ hgt_issues <- read_csv("data/working/problem_birds_height.csv")
 new_db <- bird_db_all %>% 
     slice(0) %>% 
     mutate(med_dt = double())
-
 
 
 # Fill in data base -------------------------------------------------------
@@ -52,6 +51,10 @@ for(i in 1:length(trk_files)){
     m <- mov_issues %>% 
         filter(bird_id == id_sel)
     
+    # Find problems with height
+    h <- hgt_issues %>% 
+        filter(bird_id == id_sel)
+    
     # Extract bird from database with all birds
     new_record <- bird_db_all %>% 
         filter(bird_id == id_sel)
@@ -60,13 +63,19 @@ for(i in 1:length(trk_files)){
     # Change fields that have changed when making the bird ready for modelling
     new_record <- new_record %>% 
         mutate(
-            date_start = head(trk$t_, 1),
-            date_end = tail(trk$t_, 1),
-            age = paste(unique(trk$age), collapse = ","),
+            date_start = head(trk$datetime, 1),
+            date_end = tail(trk$datetime, 1),
+            age = age,
             nloc_post = nrow(trk),
             avg_dt = mean(trk$dt, na.rm = T),
             sd_dt = sd(trk$dt, na.rm = T),
-            med_dt = median(trk$dt, na.rm = T)
+            med_dt = median(trk$dt, na.rm = T),
+            keep_loc = ifelse(!id_sel %in% loc_issues$bird_id, 1,
+                               case_when(str_detect(l$status, "Included") ~ 1,
+                                         str_detect(l$status, "Excluded") ~ 0)),
+            keep_mov = ifelse(length(m$keep) == 0, NA, m$keep),
+            keep_hgt = ifelse(length(h$keep) == 0, NA, h$keep),
+            keep_all = ifelse(sum(keep_mov, keep_hgt, keep_hgt, na.rm = T) == 3, 1, 0)
     )
     
     new_db <- rbind(new_db, new_record)
