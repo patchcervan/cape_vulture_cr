1969101193e727f60e53fd1390281a670f28f484
patchcervan <francerper@gmail.com>
2021-01-26 16:18:42 +0200

    Updated scripts in processing main

--- R/1_data_processing/main.R
+++ R/1_data_processing/main.R
@@ -7,7 +7,7 @@ rm(list = ls())
 library(tidyverse)
 
 # Load scripts
-scripts <- dir("R/data_processing")
+scripts <- dir("R/1_data_processing")
 
 # remove the "main.R" (current script)
 scripts <- scripts[str_detect(scripts, "main", negate = T)]
@@ -15,16 +15,44 @@ scripts <- scripts[str_detect(scripts, "main", negate = T)]
 # remove the creation of templates
 scripts <- scripts[str_detect(scripts, "0", negate = T)]
 
-# Run
-for(f in 1:length(scripts)){
+# Run the processing scripts for specific birds
+
+# Run processing scripts for specific birds -------------------------------
+
+proc <- scripts[1:18]
+
+for(f in 1:length(proc)){
     
     # Need to reload because of the rm functions in the scripts
-    # Load scripts
-    scripts <- dir("R/data_processing")
-    # remove the "main.R" (current script)
+    scripts <- dir("R/1_data_processing")
     scripts <- scripts[str_detect(scripts, "main", negate = T)]
-    # remove the creation of templates
     scripts <- scripts[str_detect(scripts, "0", negate = T)]
+    proc <- scripts[1:18]
     
-    source(paste0("R/data_processing/", scripts[f]))
+    print(f)
+    
+    source(paste0("R/1_data_processing/", proc[f]))
 }
+
+
+# Run other general scripts -----------------------------------------------
+
+# Run fine processing: exclude certain birds, cut out certain periods, change ages for
+# long term tracking, re-sample very high resolution tracks
+source("R/1_data_processing/3_fine_process.R")
+
+# Run the colony process scripts in which we find the central places of the 
+# vultures (WE SHOULD HAVE RAN "R/2_colony_finding/1_colony_finding.R" ALREADY)
+source("R/1_data_processing/4_colony_process.R")
+
+# Run script to classify movement modes. This takes really long, so consider
+# running in the UCT HPC. If ran in the HPC, there is a second part in the script
+# (from "Explore state proportions") that must be ran in the local script. Do it manually.
+source("R/1_data_processing/5_move_mode_process.R")
+
+# Run script to process heights and correct for heights that wrap to zero when
+# going over 2042 and other known issues
+source("R/1_data_processing/6_height_process.R")
+
+# Create a database with the new information extracted from the birds
+source("R/1_data_processing/8_create_db_fit_ready.R")
